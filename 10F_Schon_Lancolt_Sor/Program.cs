f19af8967c979f98164870a04ec7b282451f98f6
MolnAtt <molnar.attila@szlgbp.hu>
2021-10-18 11:04:23 +0200

    Add project files.

---
+++ 10F_Schon_Lancolt_Sor/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10F_Schon_Lancolt_Sor
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            /** /
+            Queue<int> sor = new Queue<int>();
+            sor.Enqueue(5); // ez a push
+            int a = sor.Dequeue(); // ez a pop
+            sor.Peek();
+            /**/
+
+            Sor<int> sor = new Sor<int>();
+            sor.Enqueue(3);
+            Console.WriteLine(sor);
+            sor.Enqueue(5);
+            Console.WriteLine(sor);
+            sor.Enqueue(2);
+            Console.WriteLine(sor);
+            sor.Enqueue(1);
+            Console.WriteLine(sor);
+            sor.Enqueue(10);
+
+            Console.WriteLine(sor);
+            Console.WriteLine(sor.Dequeue());
+            Console.WriteLine(sor);
+            Console.WriteLine(sor.Dequeue());
+            Console.WriteLine(sor);
+            Console.WriteLine(sor.Dequeue());
+            Console.WriteLine(sor);
+
+            Console.ReadKey();
+        }
+    }
+}
